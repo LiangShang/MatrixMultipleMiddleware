52a61cc9413856920c706bc769600700afa6db87
Liang Shang <liang.shang13@imperial.ac.uk>
2014-04-19 17:32:49 +0100

    Finish MatrixMultiply Server

--- matrix_multiplication.py
+++ matrix_multiplication.py
@@ -3,7 +3,7 @@ import json
 
 
 def matrix_multiply(raw_data):
-    return None
+    return multiply(*parse_raw_data(raw_data))
 
 
 def multiply(matrix1, matrix2):
@@ -11,7 +11,11 @@ def multiply(matrix1, matrix2):
     col_num = get_column_length_of(matrix2)
     result = [[0 for col in range(col_num)] for row in range(row_num)]
 
-
+    for row in range(row_num):
+        for col in range(col_num):
+            result[row][col] = \
+                sum([matrix1[row][i]*matrix2[i][col]
+                     for i in range(get_row_length_of(matrix2))])
 
     return result
 
@@ -31,6 +35,10 @@ def get_row_length_of(matrix):
 
 def get_column_length_of(matrix):
     return len(matrix)
+
+
+def parse_raw_data(raw_data):
+    return tuple(map(get_matrix_from_json, raw_data.split('<|>')))
 
 
 def get_matrix_from_json(string):
--- test/test_matrix_multiplication.py
+++ test/test_matrix_multiplication.py
@@ -37,3 +37,14 @@ class MatrixMultiplicationTest(TestCase):
 
         matrix = [[1, 1, 1], [2, 2, 2], [3, 3, 3]]
         self.assertEqual(3, get_row_length_of(matrix))
+
+    def test_multiply(self):
+        matrix1 = [[1,2,3],[4,5,6],[7,8,9]]
+        matrix2 = [[11,12,13],[14,15,16],[17,18,19]]
+        self.assertEqual([[90,96,102],[216,231,246],[342,366,390]], multiply(matrix1, matrix2))
+
+    def test_parse_raw_data(self):
+        self.assertEqual(([[1]],[[1]]), parse_raw_data('1<|>1'))
+
+        self.assertEqual(([[1,2,3]],[[4,5,6],[7,8,9]]),
+                         parse_raw_data('[1,2,3]<|>[[4,5,6],[7,8,9]]'))
